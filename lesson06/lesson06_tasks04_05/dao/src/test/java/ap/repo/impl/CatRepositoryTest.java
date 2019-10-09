5694ed41334d4af138730fe5673cc5d134b4cef9
Aliaksandr Padalka <aliaksandr_padalka@epam.com>
2019-10-09 18:27:39 +0300

    111

--- lesson06/lesson06_tasks04_05/dao/src/test/java/ap/repo/impl/CatRepositoryTest.java
+++ lesson06/lesson06_tasks04_05/dao/src/test/java/ap/repo/impl/CatRepositoryTest.java
@@ -1,9 +1,72 @@
 package ap.repo.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import ap.model.Animal;
+import ap.model.pet.Cat;
+import ap.repo.Repository;
 
 class CatRepositoryTest {
 
+  private Repository repository;
+
+  @BeforeEach
+  public void init() {
+    repository = new CatRepository();
+  }
+
+  @Test
+  public void testGet() {
+    assertEquals(null, repository.get(1));
+  }
+
+  @Test
+  public void testSaveOrUpdate() {
+    assertEquals(null, repository.get(1));
+
+    Animal animal = new Cat();
+    animal.setId(1);
+    animal.setName("Cat");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+  }
+
+  @Test
+  public void testRemove() {
+    assertEquals(null, repository.get(1));
+
+    Animal animal = new Cat();
+    animal.setId(1);
+    animal.setName("Cat");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+
+    repository.remove(animal);
+    assertEquals(null, repository.get(1));
+  }
+
+  @Test
+  public void testSaveOrUpdateNew() {
+    assertEquals(null, repository.get(1));
+
+    Animal animal = new Cat();
+    animal.setName("Cat");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+
+    repository.remove(animal);
+    assertEquals(null, repository.get(1));
+
+    animal = new Cat();
+    animal.setName("Cat");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+  }
 }
--- lesson06/lesson06_tasks04_05/dao/src/test/java/ap/repo/impl/DogRepositoryTest.java
+++ lesson06/lesson06_tasks04_05/dao/src/test/java/ap/repo/impl/DogRepositoryTest.java
@@ -1,9 +1,72 @@
 package ap.repo.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import ap.model.Animal;
+import ap.model.pet.Dog;
+import ap.repo.Repository;
 
 class DogRepositoryTest {
 
+  private Repository repository;
+
+  @BeforeEach
+  public void init() {
+    repository = new DogRepository();
+  }
+
+  @Test
+  public void testGet() {
+    assertEquals(null, repository.get(1));
+  }
+
+  @Test
+  public void testSaveOrUpdate() {
+    assertEquals(null, repository.get(1));
+
+    Animal animal = new Dog();
+    animal.setId(1);
+    animal.setName("Dog");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+  }
+
+  @Test
+  public void testRemove() {
+    assertEquals(null, repository.get(1));
+
+    Animal animal = new Dog();
+    animal.setId(1);
+    animal.setName("Dog");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+
+    repository.remove(animal);
+    assertEquals(null, repository.get(1));
+  }
+
+  @Test
+  public void testSaveOrUpdateNew() {
+    assertEquals(null, repository.get(1));
+
+    Animal animal = new Dog();
+    animal.setName("Dog");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+
+    repository.remove(animal);
+    assertEquals(null, repository.get(1));
+
+    animal = new Dog();
+    animal.setName("Dog");
+    repository.saveOrUpdate(animal);
+
+    assertEquals(animal, repository.get(1));
+  }
 }
